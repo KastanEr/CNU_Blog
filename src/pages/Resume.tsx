08165153508741d074067bd94cb338d4c899357c
KastanEr <skuller320@o.cnu.ac.kr>
2023-06-11 16:21:04 +0900

    deploy

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,20 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h2>충남대학교 컴퓨터 융합학부</h2>
+      <p>2019.03 ~ 2025.02</p>
+      <li>
+        <span className="subject">학점</span> - 4.05 / 4.5
+        <span className="subject">실전코딩</span> - github, SpringBoot, React 등의 실무 관련 지식 수강
+      </li>
+      <h2>2022 동계 실무캠프</h2>
+      <p>2022.12</p>
+      <p>to-do list 앱개발</p>
+      <p>Design sprint 방법론을 통한 기획부터 프로토타입 개발까지 진행, 서버 데이터 베이스 구축과 앱의 비즈니스 로직 개발을 담당</p>
+      <h2>To-do list App Simple</h2>
+      <p>동계 실무캠프 때 개발한 프로토타입을 바탕으로 프로젝트 진행</p>
+    </div>
+  );
 };
 
 export default Resume;
